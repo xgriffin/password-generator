2f81b3ace7980dff87a79a209fadb02fa9e74bae
xGriffin <griffinhanna24@gmail.com>
2022-01-17 16:21:57 -0600

    added password uppercase, symbols and numbers

--- develop/script.js
+++ develop/script.js
@@ -19,10 +19,22 @@ function generatePassword() {
     alert('Please make a valid selection');
     parseInt(window.prompt ('Please select password length. It must be between 8 and 128 characters'));
   }
-  // Password aoptions. Input will impact the passWordSelection array. End result will be used as a pool for random characters.
+  // Password options
   let passWordLower = window.prompt ('Would you like it to include lower case letters?');
   if (passWordLower != null) {
    passWordSelection.unshift('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z');
+  }
+  let passWordUpper = window.prompt ('Would you like it to include upper case letters?');
+  if (passWordUpper != null) {
+    passWordSelection.unshift ('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z')
+  }
+  let passWordSymbols = window.prompt ('Would you like it to include special characters?');
+  if (passWordSymbols !=null) {
+    passWordSelection.unshift ('@','#','$','%','&','(',')','{','}','!','?','[',']');
+  }
+  let passWordNums = window.prompt ('Would you like it to include numbers?');
+  if (passWordNums != null){
+    passWordSelection.unshift (1,2,3,4,5,6,7,8,9,0);
   }
 
     
