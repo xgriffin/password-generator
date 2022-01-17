f4caee34d96c5d456857184ff023c8770e1ae7d1
xGriffin <griffinhanna24@gmail.com>
2022-01-17 16:32:13 -0600

    Formatted code and updated README file

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # password-generator
+
+
+## Purpose
+To create a random password based on the criteria selected by the user.
+
+## Built With
+* HTML
+* CSS
+* JavaScript
+
+## Website
+https://xgriffin.github.io/password-generator/
+
+## Contribution
+Made by Griffin H
--- develop/script.js
+++ develop/script.js
@@ -1,46 +1,129 @@
 // Assignment code here
-let passWord = '';
+let passWord = "";
 let passWordSelection = [];
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
-    // 1. prompt the user for the password criteria
-    //    a. password length 8 < 128
-    //    b. Lowercase, uppercase, numbers, special characters
-    // 2. validate the input
-    // 3. Generate password based on criteria
-    // 4. Display password to the page.
+  // 1. prompt the user for the password criteria
+  //    a. password length 8 < 128
+  //    b. Lowercase, uppercase, numbers, special characters
+  // 2. validate the input
+  // 3. Generate password based on criteria
+  // 4. Display password to the page.
 
-    let passWordLength = parseInt(window.prompt ('Please select password length. It must be between 8 and 128 characters'));
+  let passWordLength = parseInt(
+    window.prompt(
+      "Please select password length. It must be between 8 and 128 characters"
+    )
+  );
   // Validates if user password length input is valid
   if (!passWordLength || passWordLength < 8 || passWordLength > 128) {
-    alert('Please make a valid selection');
-    parseInt(window.prompt ('Please select password length. It must be between 8 and 128 characters'));
+    alert("Please make a valid selection");
+    parseInt(
+      window.prompt(
+        "Please select password length. It must be between 8 and 128 characters"
+      )
+    );
   }
   // Password options
-  let passWordLower = window.prompt ('Would you like it to include lower case letters?');
+  let passWordLower = window.prompt(
+    "Would you like it to include lower case letters?"
+  );
   if (passWordLower != null) {
-   passWordSelection.unshift('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z');
+    passWordSelection.unshift(
+      "a",
+      "b",
+      "c",
+      "d",
+      "e",
+      "f",
+      "g",
+      "h",
+      "i",
+      "j",
+      "k",
+      "l",
+      "m",
+      "n",
+      "o",
+      "p",
+      "q",
+      "r",
+      "s",
+      "t",
+      "u",
+      "v",
+      "w",
+      "x",
+      "y",
+      "z"
+    );
   }
-  let passWordUpper = window.prompt ('Would you like it to include upper case letters?');
+  let passWordUpper = window.prompt(
+    "Would you like it to include upper case letters?"
+  );
   if (passWordUpper != null) {
-    passWordSelection.unshift ('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z')
+    passWordSelection.unshift(
+      "A",
+      "B",
+      "C",
+      "D",
+      "E",
+      "F",
+      "G",
+      "H",
+      "I",
+      "J",
+      "K",
+      "L",
+      "M",
+      "N",
+      "O",
+      "P",
+      "Q",
+      "R",
+      "S",
+      "T",
+      "U",
+      "V",
+      "W",
+      "X",
+      "Y",
+      "Z"
+    );
   }
-  let passWordSymbols = window.prompt ('Would you like it to include special characters?');
-  if (passWordSymbols !=null) {
-    passWordSelection.unshift ('@','#','$','%','&','(',')','{','}','!','?','[',']');
+  let passWordSymbols = window.prompt(
+    "Would you like it to include special characters?"
+  );
+  if (passWordSymbols != null) {
+    passWordSelection.unshift(
+      "@",
+      "#",
+      "$",
+      "%",
+      "&",
+      "(",
+      ")",
+      "{",
+      "}",
+      "!",
+      "?",
+      "[",
+      "]"
+    );
   }
-  let passWordNums = window.prompt ('Would you like it to include numbers?');
-  if (passWordNums != null){
-    passWordSelection.unshift (1,2,3,4,5,6,7,8,9,0);
+  let passWordNums = window.prompt("Would you like it to include numbers?");
+  if (passWordNums != null) {
+    passWordSelection.unshift(1, 2, 3, 4, 5, 6, 7, 8, 9, 0);
   }
 
-  for (let i = 0; i<=passWordLength-1; i++ ) {
-    passWord  += passWordSelection[Math.floor(Math.random() * passWordSelection.length)];
+  for (let i = 0; i <= passWordLength - 1; i++) {
+    passWord +=
+      passWordSelection[Math.floor(Math.random() * passWordSelection.length)];
   }
-    return passWord;
+  return passWord;
 }
 
 // Write password to the #password input
@@ -49,7 +132,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
